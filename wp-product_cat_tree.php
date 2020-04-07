926f958d2c8b628207ed69bac67156986482f171
areut-su <areut.su@gmail.com>
2020-04-07 14:36:01 +0700

    прокинут post_id

--- wp-product_cat_tree.php
+++ wp-product_cat_tree.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WP Product Cat Tree
  * Description: Выводит дерево  категорий (product_cat) в цикле  Wp
- * use [PRODUCT_CAT_TREE level=1] or [PRODUCT_CAT_TREE] or product_cat_tree_print(['level'=>1])
+ * use [PRODUCT_CAT_TREE level=1 post_id =1111] or [PRODUCT_CAT_TREE] or product_cat_tree_print(['level'=>1])
  * Author:      areut
  * Author URI:  https://github.com/
  * Version:     0.1
@@ -17,7 +17,7 @@ add_shortcode('PRODUCT_CAT_TREE', 'product_cat_tree_print');
 /**
  *
  * @param array $atts
- *   product_cat_tree_print(['level'=>1]);
+ *   product_cat_tree_print(['level'=>1,'post_id'=>3793]);
  */
 function product_cat_tree_print($atts)
 {
@@ -28,9 +28,23 @@ function product_cat_tree_print($atts)
     }
     $level = isset($atts['level']) ? $atts['level'] : 1;
 
+    $post_id = isset($atts['post_id']) ? $atts['level'] : get_the_ID();
+
     try
     {
-        $m = CatTree::create(get_categories(['taxonomy' => 'product_cat',]), $level);
+        $m = [];
+        if (!empty($post_id))
+        {
+
+
+            $m = CatTree::create(
+                get_categories(
+                    [
+                        'taxonomy' => 'product_cat',
+                        'object_ids' => $post_id,
+                    ]
+                ), $level);
+        }
     }
     catch (Exception $e)
     {
